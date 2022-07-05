071f72fbdea5fabe9d60eedcaaa85f62441ab399
KANGDONGHOON0413 <kmerits@gmail.com>
2022-07-05 20:58:42 +0900

    초기 커밋.

---
+++ day22_02/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace day22_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.ans_out = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(43, 48);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(61, 24);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "사과";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(43, 78);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(46, 24);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "배";
+            this.checkBox2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(43, 108);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(61, 24);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "수박";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 84);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "확인";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(287, 27);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "자신이 좋아하는 과일을 모두 선택하시오";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(24, 138);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 65);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "설문 만족도";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(103, 23);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(75, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "불만족";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(20, 23);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(60, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "만족";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // ans_out
+            // 
+            this.ans_out.Location = new System.Drawing.Point(24, 201);
+            this.ans_out.Name = "ans_out";
+            this.ans_out.Size = new System.Drawing.Size(281, 27);
+            this.ans_out.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ans_out);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "설문";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox ans_out;
+    }
+}
+
---
+++ day22_02/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day22_02
+{
+    public partial class Form1 : Form
+    {
+        int cntnum1 = 0, cntnum2 = 0, cntnum3 = 0;
+        int cntgood = 0, cntbad = 0;
+        int cntnum = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Width = 400;
+            Height = 300;
+            ShowGoodBad();
+        }
+
+        //check button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CntAndShowAns();
+                ShowGoodBad();
+            }
+            catch (IndexOutOfRangeException)
+            {
+                MessageBox.Show("과일을 선택하지 않으셨습니다.");
+            }
+            finally
+            {
+            ReSet();
+            
+            Invalidate();
+            }          
+        }
+
+        public void CntAndShowAns()
+        {
+            Queue<string> q = new Queue<string>();
+            string ans = "당신이 선호한 상품: ";
+            if (checkBox1.Checked) { q.Enqueue("사과"); cntnum1++; }
+            else if (checkBox2.Checked) { q.Enqueue("배"); cntnum2++; }
+            else if (checkBox3.Checked) { q.Enqueue("수박"); cntnum2++; }
+            else throw new IndexOutOfRangeException();
+            int cnt = q.Count;
+
+            for (int i = 0; i < cnt; i++)
+            {
+                if (i != 0) ans += ", ";
+                ans += q.Dequeue();
+            }
+            MessageBox.Show(ans + "입니다.");
+
+            if (radioButton1.Checked) cntgood += 1;
+            if (radioButton2.Checked) cntbad += 1;
+            cntnum++;
+        }
+
+        public void ShowGoodBad()
+        {         
+            string gdbd = "만족: " + cntgood + "\t불만족: " + cntbad + "\t참여인원: " + cntnum;
+            ans_out.Text = gdbd;
+        }
+        
+        void ReSet()
+        {
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Width = 400;
+            Height = 300;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("설문을 종료하시겠습니까", "설문", MessageBoxButtons.YesNo) == DialogResult.No) e.Cancel = true;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show($"사과: {cntnum1}\n 배: {cntnum2}\n수박: {cntnum3}\n\t참여인원: {cntnum}","설문 결과");
+        }
+    }
+}
+    
